79c7859716d194c862be3f41592973181352aa46
khayalnb_997 <xeyal.binnetov@profit.az>
2023-01-07 12:32:51 +0400

    added function by id hospital type

--- HMS.WebApi/Controllers/HospitalTypeController.cs
+++ HMS.WebApi/Controllers/HospitalTypeController.cs
@@ -3,6 +3,7 @@ using HMS.DAL.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace HMS.WebApi.Controllers
 {
@@ -13,28 +14,38 @@ namespace HMS.WebApi.Controllers
         private readonly IHospitalTypeRepository _hospitalTypeRepository;
         public HospitalTypeController(IHospitalTypeRepository hospitalTypeRepository)
         {
-            _hospitalTypeRepository= hospitalTypeRepository;
+            _hospitalTypeRepository = hospitalTypeRepository;
         }
         [HttpPost]
         public IActionResult Create(HospitalType hospitalType)
         {
-            if (hospitalType!=null)
+            if (hospitalType != null)
             {
-                _hospitalTypeRepository.AddHospitalType(hospitalType);
+                _hospitalTypeRepository.CreateHospitalType(hospitalType);
                 return Ok(hospitalType);
             }
-            return BadRequest();    
+            return BadRequest();
         }
         [HttpGet]
         public IActionResult GetAll()
         {
             var hospitalTypeList = _hospitalTypeRepository.GetAllHospitalTypes();
-            if (hospitalTypeList!=null && hospitalTypeList.Any())
+            if (hospitalTypeList != null && hospitalTypeList.Any())
             {
                 return Ok(hospitalTypeList);
 
             }
             return NoContent();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetHospitalTypeById(int? id)
+        {
+            if (id!=null)
+            {
+                var hospitalType = _hospitalTypeRepository.GetHospitalTypeById(id);
+                return Ok(hospitalType);
+            }
+            return NotFound();
         }
     }
 }
